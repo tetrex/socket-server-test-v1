e295c811b3bb46088530cfde8e120460b91b4995
piyush_applore <piyush@applore.in>
2021-10-05 12:35:26 +0530

    working socket

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const io = new Server({});
 io.on("connection", (socket) => {
 	console.log("connected NEW SOCKET : ", socket.id);
 	socket.on("click", () => {
+		console.log('click emit');
 		socket.emit("response", "Click OK");
 	});
 	socket.on("test", () => {
+		console.log('test emit');
 		socket.emit("response", "TEST OK");
 	});
 	socket.on("data", (data) => {
-		let { name, value } = data;
+		console.log('data emit', data);
+		let { name, value } = JSON.parse(data);
+		console.log(name, value);
 		socket.emit("response", `did u pass ? name : ${name}   value : ${value}`);
 	});
 	socket.on("disconnect", (reason) => {
